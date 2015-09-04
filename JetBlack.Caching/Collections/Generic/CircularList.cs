0700633208a23ac32b0d40f7a620f84c033a2351
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-09-04 10:40:55 +0100

    Added an IList<T> implementation of the circular buffer.

---
+++ JetBlack.Caching/Collections/Generic/CircularList.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace JetBlack.Caching.Collections.Generic
+{
+    public class CircularList<T> : CircularBuffer<T>, IList<T>
+    {
+        public CircularList(int capacity)
+            : base(capacity)
+        {
+        }
+
+        public void Add(T item)
+        {
+            Enqueue(item);
+        }
+
+        public bool Contains(T item)
+        {
+            return this.Any(x => Equals(x, item));
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            foreach (var value in this)
+                array[arrayIndex++] = value;
+        }
+
+        public bool Remove(T item)
+        {
+            var index = IndexOf(item);
+            if (index == -1) return false;
+            RemoveAt(index);
+            return true;
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+    }
+}
